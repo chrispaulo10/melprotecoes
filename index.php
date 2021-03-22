07a2e0f1a2e5533081bcc5392ee1344f8a22fca9
Christian Paulo <christian.paulo54@gmail.com>
2021-03-22 13:45:34 -0300

    muitas cidades feitas

--- index.php
+++ index.php
@@ -3,17 +3,54 @@
 
     $rotas = array(
         "" => "index",
-        "Início" => "index",
+        "home" => "index",
         "404" => "404",
         "blog" => [
             "" => "blog",
             "detalhes" => "blog-details"
         ],
-        "Cerca-de-Piscina" => $prefixo."cerca-piscina",
-        "Redes-de-Proteção-Zona-Leste" => $prefixo."protecao-zona-leste",
-        "Redes-de-Proteção-Zona-Oeste" => $prefixo."protecao-zona-oeste",
-        "Redes-de-Proteção" => $prefixo."redes-de-protecao",
-        "Tela-para-Pets" => $prefixo."tela-gatos",
+        "redes-de-protecao" => $prefixo."redes-de-protecao",
+        "cerca-de-piscina" => $prefixo."cerca-piscina",
+        "tela-para-pets" => $prefixo."tela-gatos",
+        "mosquiteira" => $prefixo."mosquiteira",
+        "limitadores" => $prefixo."limitadores",
+        "redes-esportivas" => $prefixo."redes-esportivas",
+        "grades" => $prefixo."grades",
+        "capas" => $prefixo."capas",
+        // CIDADES DA ZONA LESTE
+        "redes-de-protecao-na-zona-leste" => $prefixo."protecao-zona-leste",
+        "redes-de-protecao-em-aricanduva" => $prefixo."protecao-zona-leste",
+        "redes-de-protecao-em-arthur-alvim" => $prefixo."protecao-zona-leste",
+        "redes-de-protecao-em-belenzinho" => $prefixo."protecao-zona-leste",
+        "redes-de-protecao-em-cangaíba" => $prefixo."protecao-zona-leste",
+        "redes-de-protecao-no-carrão" => $prefixo."protecao-zona-leste",
+        "redes-de-protecao-em-cidade-tiradentes" => $prefixo."protecao-zona-leste",
+        "redes-de-protecao-em-cidade-ermelino-matarazzo" => $prefixo."protecao-zona-leste",
+        "redes-de-protecao-em-guaianazes" => $prefixo."protecao-zona-leste",
+        "redes-de-protecao-em-itaim-paulista" => $prefixo."protecao-zona-leste",
+        "redes-de-protecao-em-itaquera" => $prefixo."protecao-zona-leste",
+        "redes-de-protecao-no-jardim-analia-franco" => $prefixo."protecao-zona-leste",
+        "redes-de-protecao-na-mooca" => $prefixo."protecao-zona-leste",
+        "redes-de-protecao-em-patriarca" => $prefixo."protecao-zona-leste",
+        "redes-de-protecao-na-penha" => $prefixo."protecao-zona-leste",
+        "redes-de-protecao-em-sao-mateus" => $prefixo."protecao-zona-leste",
+        "redes-de-protecao-em-sao-mateus" => $prefixo."protecao-zona-leste",
+        // CIDADES DA ZONA OESTE
+        "redes-de-protecao-na-zona-oeste" => $prefixo."protecao-zona-oeste",
+        // CIDADES DA ZONA SUL
+
+        // CIDADES DA ZONA NORTE
+
+        // CIDADES DO CENTRO SP
+        
+        // CIDADES DO ABCD
+
+        // CIDADES DE GUARULHOS
+
+        // CIDADES DO INTERIOR
+
+        // CIDADES NO LITORAL SP
+
     //  "URL" => "NOME ARQUIVO"
     );
 
