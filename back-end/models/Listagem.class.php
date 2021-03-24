1ff41006ec3f5fed4dbb5908d7c3fccd78c50107
KelpGF <kelpgf@gmail.com>
2021-03-24 17:10:26 -0300

    version rotas

--- back-end/models/Listagem.class.php
+++ back-end/models/Listagem.class.php
@@ -1,5 +1,5 @@
 <?php
-    require_once "../database/Conexao.class.php";
+    require_once "back-end/database/Conexao.class.php";
 
     class Listagem {
 
@@ -39,7 +39,22 @@
 
                             array_push($dados, $regiao);
                         }
-                    } else $dados = $regioes;
+                    } else if ($cidades) {
+                        $cidades = $this->cidades(false, $cidades);
+
+                        foreach ($regioes as $indice => $regiao) {
+                            $regiao['cidades'] = [];
+
+                            foreach ($cidades as $indice => $cidade) {
+                                if ($cidade['id_regiao_fk'] == $regiao['id']) {
+                                    array_push($regiao['cidades'], $cidade);
+                                }
+                            }
+
+                            array_push($dados, $regiao);
+                        }
+                    }
+                    else $dados = $regioes;
                 } else $dados = self::SEM_REGISTROS;
 
                 return $dados;
@@ -101,16 +116,30 @@
 
         /*======================================================================================*/
 
-        public function cidades($id_estado = 0) {
+        public function cidades($id_estado = 0, $regiao = false) {
             $conexao = new Conexao();
             $connection = $conexao->conectar();
 
             try {
-                $sql = "SELECT * FROM `cidades`";
-                ($id_estado > 0) && $sql .= "WHERE id_estado_fk = :id_estado";
+                $colunas = '*';
+
+                ($id_estado == 0 && $regiao) && $colunas = "id_regiao_fk, cidades.nome";
+
+                $sql = "SELECT $colunas FROM `cidades`";
+
+                if ($id_estado > 0) {
+                    $sql .= " WHERE id_estado_fk = :id_estado";
+                } else if ($regiao) {
+                    $sql .= " INNER JOIN estados ON estados.id = id_estado_fk	
+                            INNER JOIN regioes ON regioes.id = id_regiao_fk
+                    ";
+                }
 
                 $consulta = $connection->prepare($sql);
-                ($id_estado > 0) && $consulta->bindParam("id_estado", $id_estado);
+
+                if ($id_estado > 0) {
+                    ($id_estado > 0) && $consulta->bindParam("id_estado", $id_estado);
+                }
 
                 $consulta->execute();
 
