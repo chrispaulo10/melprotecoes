32f205e04d6917c35de35865f647933286523345
KelpGF <kelpgf@gmail.com>
2021-03-26 16:08:40 -0300

    ajuste js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -66,6 +66,18 @@ $(document).ready(function () {
 
     const url_atual = window.location.href;
     let page = url_atual.split("/");
+    let titulo = page[page.length - 1].split("-");
+    let local = "";
+
+    for (let index = 3; index < titulo.length; index++) {
+        local += `${titulo[index]} `;
+    }
+
+    $.each(chars_especiais, function (codigo, char) {
+        local = local.replace(codigo, char);
+    });
+
+    $(".local").text(local);
 
     if (page[page.length - 1] == "" || page[page.length - 1] == 'home') {
         $.get("back-end/controller/ControllerListagem.php", function (retorno) {
