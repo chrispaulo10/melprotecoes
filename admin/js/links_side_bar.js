f5ae6c3170ec4975396061932e2adafd9ac37ef5
chrispaulo10 <christian.paulo54@gmail.com>
2021-08-25 19:27:01 -0300

    enviei pra hospedagem

--- admin/js/links_side_bar.js
+++ admin/js/links_side_bar.js
@@ -1,23 +1,23 @@
 // function para redirecionar as paginas
 function redirect(event, page, title) {
     event.preventDefault();
-    document.title = "Administrador - "+title;
-    $("#main-content").load(page+'.html');
+    document.title = "Administrador - " + title;
+    $("#main-content").load(page + '.html');
 }
 
-$(document).ready(function() {    
+$(document).ready(function () {
     document.title = "Administrador - Páginas de Conteúdo";
     $("#main-content").load('link_page.html');
 
     $(".link_conteudos").click(function (event) {
-        redirect(event, "conteudo", "Páginas de Conteúdo");  
-    }); 
+        redirect(event, "conteudo", "Páginas de Conteúdo");
+    });
 
     $(".link_categorias").click(function (event) {
-        redirect(event, "categorias", "Categorias");  
-    }); 
+        redirect(event, "categorias", "Categorias");
+    });
 
     $(".link_page").click(function (event) {
-        redirect(event, "link_page", "Links");  
-    }); 
+        redirect(event, "link_page", "Links");
+    });
 })
